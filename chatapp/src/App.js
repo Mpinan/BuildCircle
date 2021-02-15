233ed356b180c4d1240670c1706d938a5ba97de8
jaitone <jaitoone@gmail.com>
2021-02-15 12:28:25 +0000

    set up skeleton

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -2,17 +2,33 @@ import React, { Component } from "react";
 import PubNub from 'pubnub';
 import { PubNubProvider, usePubNub } from 'pubnub-react';
 
+
+// Create a new instance of PubNub to set it up with personalized keys
 const pubnub = new PubNub({
   publishKey: 'pub-c-22ec4f77-ccd2-4814-8829-ef38cedbad62',
   subscribeKey: 'pub-c-22ec4f77-ccd2-4814-8829-ef38cedbad62'
 });
 
 class App extends Component {
+  state = {
+    message: "",
+    messages: []
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-        hello world 
-      </div>
+      <PubNubProvider client={pubnub}>
+        <div>
+          <input
+            type="text"
+          />
+          <button>
+            Send
+          </button>
+        </div>
+      </PubNubProvider>
     );
   }
 }
