963a685f0b5851eb30a9bf29b0f4f3cefa1a827c
jaitone <jaitoone@gmail.com>
2021-02-15 12:56:14 +0000

    Added basic chat implementation, further skeleton, you can enter a messages and keep in state and show it on doc

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -6,7 +6,8 @@ import { PubNubProvider, usePubNub } from 'pubnub-react';
 // Create a new instance of PubNub to set it up with personalized keys
 const pubnub = new PubNub({
   publishKey: 'pub-c-22ec4f77-ccd2-4814-8829-ef38cedbad62',
-  subscribeKey: 'pub-c-22ec4f77-ccd2-4814-8829-ef38cedbad62'
+  subscribeKey: 'pub-c-22ec4f77-ccd2-4814-8829-ef38cedbad62',
+  uuid: 'Mario'
 });
 
 class App extends Component {
@@ -16,17 +17,42 @@ class App extends Component {
   }
 
 
+  onChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+  
+  addMessageToWindow(message){
+    let messages = [...this.state.messages]
+    messages.push(message)
+    this.setState( {messages } )
+  }
+  
+  handleSubmit() {
+    this.addMessageToWindow(this.state.message)
+  }
 
   render() {
+    const {messages} = this.state
     return (
       <PubNubProvider client={pubnub}>
         <div>
           <input
+            onChange={this.onChange.bind(this)}
             type="text"
+            name="message"
           />
-          <button>
+          <button onClick={this.handleSubmit.bind(this)} >
             Send
           </button>
+        </div>
+        <div>
+          {messages.map((message, index) => {
+            return (
+              <div key={`message-${index}`}>
+                {message}
+              </div>
+            );
+          })}
         </div>
       </PubNubProvider>
     );
