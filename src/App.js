bfb74a921e6c8169847c5e3ab0cdd310b8f10df0
jaitone <jaitoone@gmail.com>
2021-02-18 16:58:29 +0000

    Added default user

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ function App() {
   const [name, setName] = useState('');
   console.log(name)
 
+  const handleName = (name = "Default User") => {
+    if(name) {return name}
+  }
+
   const pubnub = new PubNub({
     publishKey: 'pub-c-22ec4f77-ccd2-4814-8829-ef38cedbad62',
     subscribeKey: 'sub-c-14b19f88-6f6f-11eb-a2ab-226faaaba132',
-    uuid: name
+    uuid: handleName(name)
   });
+
 
   return (
       <PubNubProvider client={pubnub}>
